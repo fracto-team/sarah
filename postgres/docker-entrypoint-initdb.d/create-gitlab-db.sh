10ab283f0f9c0fc36dbd76ed263ee1c386398cf6
Eray AYDIN <erayaydin2016@gmail.com>
2020-03-04 11:39:46 +0300

    First initialize of the Sarah! :tada:

---
+++ postgres/docker-entrypoint-initdb.d/create-gitlab-db.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Copy create-x-db.sh.example to create-x-db.sh
+# then uncomment, set database name and username to create databases
+
+# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+#     CREATE USER db1 WITH PASSWORD 'db1';
+#     CREATE DATABASE db1;
+#     GRANT ALL PRIVILEGES ON DATABASE db1 TO db1;
+# EOSQL
+
+if [ "$GITLAB_POSTGRES_INIT" == 'true' ]; then
+	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+		CREATE USER $GITLAB_POSTGRES_USER WITH PASSWORD '$GITLAB_POSTGRES_PASSWORD';
+		CREATE DATABASE $GITLAB_POSTGRES_DB;
+		GRANT ALL PRIVILEGES ON DATABASE $GITLAB_POSTGRES_DB TO $GITLAB_POSTGRES_USER;
+		ALTER ROLE $GITLAB_POSTGRES_USER CREATEROLE SUPERUSER;
+	EOSQL
+	echo
+fi
+
